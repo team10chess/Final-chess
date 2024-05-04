0c8ad1ce99b6ddd7761d1f636df6bf695352f86e
team10chess <157992396+team10chess@users.noreply.github.com>
2024-05-04 07:07:38 +0530

    Update server.js

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,5 +1,5 @@
-export const server = "http://localhost:8000/api/v2";
+export const server = "https://final-chess-backend.vercel.app/api/v2";
 
-export const backend_url="http://localhost:8000/"
+export const backend_url="https://final-chess-backend.vercel.app/"
 
 

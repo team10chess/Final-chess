05993849fdc162d81d6bbea01d565edf77a60d82
NirajDalavi <niraj.dalavi@gmail.com>
2024-05-13 23:54:10 +0530

    Co-authored-by: Bhuvanchandra M <Bhuvanchandram@users.noreply.github.com>

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,9 +1,9 @@
-// export const server = "http://localhost:8000/api/v2";
+export const server = "http://localhost:8000/api/v2";
 
-// export const backend_url="http://localhost:8000/"
+export const backend_url="http://localhost:8000/"
 
 
-export const server = "https://final-chess.vercel.app//api/v2";
+// export const server = "https://final-chess.vercel.app//api/v2";
 
-export const backend_url="https://final-chess.vercel.app/"
+// export const backend_url="https://final-chess.vercel.app/"
 
